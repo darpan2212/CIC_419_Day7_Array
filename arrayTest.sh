51705c5270c5f9372e3ae96efeccb09d22fdadd9
Darpan Madlani <Darpan@proclink.com>
2021-12-16 08:27:11 +0530

    Array test example developed

---
+++ arrayTest.sh
@@ -0,0 +1,20 @@
+cnt=0;
+animal[((cnt++))]="Lion";
+animal[((cnt++))]="Tiger";
+animal[((cnt++))]="Dog";
+animal[((cnt++))]="Lion";
+animal[((cnt++))]="Tiger";
+animal[((cnt++))]="Dog";
+animal[((cnt++))]="Lion";
+animal[((cnt++))]="Tiger";
+animal[((cnt++))]="Dog";
+animal[((cnt++))]="Lion";
+animal[((cnt++))]="Tiger";
+animal[((cnt++))]="Dog";
+
+echo ${animal[@]};
+echo "Array size of animal : ${#animal[@]}"
+bird=("Pigeon" "Crow" "Eagle");
+echo ${bird[@]};
+
+echo ${animal[1]};
